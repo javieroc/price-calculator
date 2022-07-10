83bc7a2ead412168b1d53de9a39a158a6a592a0d
Javier Ocampo <ferroxido@gmail.com>
2022-07-10 06:04:06 -0300

    Setup repository :rocket:

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'airbnb/hooks',
+        'airbnb-typescript'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            'jsx': true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        tsconfigRootDir: __dirname,
+        project: './tsconfig.json'
+    },
+    ignorePatterns: ['.eslintrc.js', 'vite.config.ts'],
+    plugins: [
+        'react',
+        'react-hooks',
+        '@typescript-eslint'
+    ],
+    rules: {
+        'import/prefer-default-export': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/require-default-props': 'off',
+        'react/no-unstable-nested-components': 'off'
+    },
+}
